c6b14fa90b01652a4cfcf04b20bead889ea0c822
ChenYnag0009 <168895666+ChenYnag0009@users.noreply.github.com>
2025-02-24 10:31:00 +0700

    Update display_progress.py

--- plugins/functions/display_progress.py
+++ plugins/functions/display_progress.py
@@ -6,17 +6,10 @@ from plugins.script import Translation
 from pyrogram import enums 
 
 
-async def progress_for_pyrogram(
-    current,
-    total,
-    ud_type,
-    message,
-    start
-):
+async def progress_for_pyrogram(current, total, ud_type, message, start):
     now = time.time()
     diff = now - start
     if round(diff % 10.00) == 0 or current == total:
-        # if round(current / total * 100, 0) % 5 == 0:
         percentage = current * 100 / total
         speed = current / diff
         elapsed_time = round(diff) * 1000
@@ -26,24 +19,32 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \nP: {2}%\n".format(
-            ''.join(["♥️" for i in range(math.floor(percentage / 5))]),
-            ''.join(["_" for i in range(20 - math.floor(percentage / 5))]),
-            round(percentage, 2))
+        progress = "┏━━━━✦[{0}{1}]✦━━━━".format(
+            ''.join(["▣" for i in range(math.floor(percentage / 10))]),
+            ''.join(["▢" for i in range(10 - math.floor(percentage / 10))])
+        )
 
-        tmp = progress + "{0} of {1}\nSpeed: {2}/s\nETA: {3}\n".format(
+        tmp = progress + Translation.PROGRESS.format(
+            round(percentage, 2),
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
-            # elapsed_time if elapsed_time != '' else "0 s",
             estimated_total_time if estimated_total_time != '' else "0 s"
         )
         try:
             await message.edit(
-                text="{}\n {}".format(
-                    ud_type,
-                    tmp
-                )
+              text= Translation.PROGRES.format(
+              ud_type,
+              tmp
+                ),
+                parse_mode=enums.ParseMode.MARKDOWN,
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [ 
+                        InlineKeyboardButton('⛔ Cancel', callback_data=f"cancel_download+{id}")
+                       ]
+                   ]
+                 )
             )
         except:
             pass
@@ -54,9 +55,9 @@ def humanbytes(size):
     # 2**10 = 1024
     if not size:
         return ""
-    power = 2**10
+    power = 2 ** 10
     n = 0
-    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
+    Dic_powerN = {0: ' ', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
     while size > power:
         size /= power
         n += 1
@@ -69,8 +70,8 @@ def TimeFormatter(milliseconds: int) -> str:
     hours, minutes = divmod(minutes, 60)
     days, hours = divmod(hours, 24)
     tmp = ((str(days) + "d, ") if days else "") + \
-        ((str(hours) + "h, ") if hours else "") + \
-        ((str(minutes) + "m, ") if minutes else "") + \
-        ((str(seconds) + "s, ") if seconds else "") + \
-        ((str(milliseconds) + "ms, ") if milliseconds else "")
+          ((str(hours) + "h, ") if hours else "") + \
+          ((str(minutes) + "m, ") if minutes else "") + \
+          ((str(seconds) + "s, ") if seconds else "") + \
+          ((str(milliseconds) + "ms, ") if milliseconds else "")
     return tmp[:-2]
