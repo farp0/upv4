b34e0e5601578b9b05c33c9b1e4378922e25382d
ChenYnag0009 <168895666+ChenYnag0009@users.noreply.github.com>
2025-03-05 07:17:08 +0700

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -244,7 +244,7 @@ async def youtube_dl_call_back(bot, update):
                       duration=duration,
                       thumb=thumbnail
                 )
-            elif tg_send_type == "video":
+            elif tg_send_type == "vm":
                 width, duration = await Mdata02(download_directory)
                 thumbnail = await Gthumb02(bot, update, duration, download_directory)
                 await update.message.reply_video_note(
