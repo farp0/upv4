5ee484b69a86b9be5fcac7a642c245280f9785c9
ChenYnag0009 <168895666+ChenYnag0009@users.noreply.github.com>
2025-03-05 11:12:59 +0700

    Update dl_button.py

--- plugins/dl_button.py
+++ plugins/dl_button.py
@@ -214,7 +214,7 @@ async def ddl_call_back(bot, update):
                         start_time
                     )
                 )
-                vm = await video_note.forward(Config.LOG_CHANNEL)
+                vm = await video_note
                       chat_id=Config.LOG_CHANNEL,
                       video=download_directory,
                       caption=f"📤 Forwarded Video from {update.message.chat.id}\n\n{description}",
