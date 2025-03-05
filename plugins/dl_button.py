453bdf9785846ce16e3ce57c086844a1f37aaf4a
ChenYnag0009 <168895666+ChenYnag0009@users.noreply.github.com>
2025-03-05 11:45:35 +0700

    Update dl_button.py

--- plugins/dl_button.py
+++ plugins/dl_button.py
@@ -199,7 +199,7 @@ async def ddl_call_back(bot, update):
                       duration=duration,
                       thumb=thumbnail
                 )
-           elif tg_send_type == "vm":
+            elif tg_send_type == "vm":
                 width, duration = await Mdata02(download_directory)
                 thumbnail = await Gthumb02(bot, update, duration, download_directory)
                 await update.message.reply_video_note(
