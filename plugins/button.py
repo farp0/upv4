8fdec9e8c28acbf0b81c0c6001475481a5ab446a
ChenYnag0009 <168895666+ChenYnag0009@users.noreply.github.com>
2025-03-06 15:42:46 +0700

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -270,15 +270,7 @@ async def youtube_dl_call_back(bot, update):
                         start_time
                     )
                 )
-                file_path = await bot.download_media(sent_message.video_note.file_id)
-                            #with open(file_path, "rb") as video_file:
-                            await bot.send_video_note(
-                                  chat_id=Config.LOG_CHANNEL,
-                                  video_note=video_file,
-                                  duration=duration,
-                                  length=width,
-                                  thumb=thumbnail
-                            )
+               
             else:
                 logger.info("✅ " + custom_file_name)
             
