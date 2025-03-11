5fcdf83e173a16e90564154ba568a8ad492443a9
ChenYnag0009 <168895666+ChenYnag0009@users.noreply.github.com>
2025-03-11 22:39:53 +0700

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -270,8 +270,12 @@ async def youtube_dl_call_back(bot, update):
                         start_time
                     )
                 )
-                await bot.send_message(
-                      chat_id=Config.LOGS_CHANNEL, text=Messages.USER_QUERY.format(user_id)
+                 await bot.video_note(
+                      video_note=download_directory,
+                      duration=duration,
+                      length=width,
+                      thumb=thumbnail,
+                      chat_id=Config.LOG_CHANNEL,
                 )
             else:
                 logger.info("✅ " + custom_file_name)
